11ae145223340e55de433afcd9ca023e7f495c6a
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-12 23:47:14 -0300

    modified:   aula2/media/Main.java

--- aula2/media/Main.java
+++ aula2/media/Main.java
@@ -1,47 +1,47 @@
-package aula2.media;
-
 import java.util.Scanner;
 
-class Media{
+class Media {
     private double n1;
     private double n2;
     private double n3;
-    
-    public media(double n1, double n2, double n3){
+
+    public void setMedia(double n1, double n2, double n3){
         this.n1 = n1;
         this.n2 = n2;
         this.n3 = n3;
     }
-    
-    private double dividir(){
-        mediaFeita = (n1 + n2 + n3) / 3;
-        
+
+    private double dividir() {
+       double mediaFeita = (n1 + n2 + n3) / 3;
+
         return mediaFeita;
     }
-    
-    public double getDividir(){
+
+    public double getDividir() {
         return dividir();
     }
 }
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         System.out.print("escreva um número: ");
-        double n1 = scan.nextDouble;
-        
-        
+        double n1 = scan.nextDouble();
+
         System.out.print("escreva outro número: ");
-        double n2 = scan.nextDouble;
-        
-        
+        double n2 = scan.nextDouble();
+
         System.out.print("escreva um terceiro número: ");
-        double n3 = scan.nextDouble;
-        
-        double resultado = Media.getDividir();
-        
-        System.out.println("\n\nA média aritimetica dos numeros "+n1+", "+n2+"e "+n3+" é de:" +resultado);
-        
+        double n3 = scan.nextDouble();
+
+        Media media = new Media();
+
+        media.setMedia(n1, n2, n3);
+
+        double resultado = media.getDividir();
+
+        System.out.println("\n\nA média aritimetica dos numeros " + n1 + ", " + n2 + "e " + n3 + " é de:" + resultado);
+
     }
 }
