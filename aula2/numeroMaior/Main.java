e42617614f8a25c4b588fd415ee50977ce2b200f
Marcelo Lisboa <161672293+oLisboaaa@users.noreply.github.com>
2024-09-13 10:58:40 -0300

    Update Main.java

--- aula2/numeroMaior/Main.java
+++ aula2/numeroMaior/Main.java
@@ -1,5 +1,65 @@
 package aula2.numeroMaior;
 
-public class Main {
+import java.util.Scanner;
+
+class numeroMaior{
     
+    private double n1;
+    private double n2;
+    private double n3;
+    
+    
+    public void setNumeroMaior(double n1, double n2, double n3){
+    
+    this.n1 = n1;
+    this.n2 = n2;
+    this.n3 = n3;
+    }
+    
+    
+    private double checkNum(){
+        
+        double maior;
+        
+        if (n2 < n1 && n3 < n1) {
+            maior = n1;
+        } else if (n1 < n2 && n3 < n2) {
+            maior = n2;
+        } else {
+            maior = n3;
+        }
+        
+        return maior;
+    }
+    
+    public double getCheckNum(){
+        return checkNum();
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+        System.out.print("escreva um número: ");
+        double n1 = scan.nextDouble();
+
+        System.out.print("escreva outro número: ");
+        double n2 = scan.nextDouble();
+
+        System.out.print("escreva um terceiro número: ");
+        double n3 = scan.nextDouble();
+        
+        numeroMaior numero = new numeroMaior();
+        
+        numero.setNumeroMaior(n1, n2, n3);
+        
+        double resultado = numero.getCheckNum();
+        
+        
+        System.out.print("\n\n o maior numero entre: "+n1+", "+n2+" e "+n3+" é o numero: "+resultado);
+        
+        scan.close();
+	}
 }
