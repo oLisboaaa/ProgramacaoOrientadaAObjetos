fe1fd4ce49aac15cc5f91ef8edcecf1be89ef5e0
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-12 23:48:57 -0300

    modified:   aula2/media/Main.java

--- aula2/media/Main.java
+++ aula2/media/Main.java
@@ -5,7 +5,7 @@ class Media {
     private double n2;
     private double n3;
 
-    public void setMedia(double n1, double n2, double n3){
+    public void setMedia(double n1, double n2, double n3) {
         this.n1 = n1;
         this.n2 = n2;
         this.n3 = n3;
