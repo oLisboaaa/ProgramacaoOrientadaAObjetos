0de50ba8d8c0443bbf4e4e8a553c72d2ae39533f
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-15 14:12:06 -0300

    new file:   aula3/filme/Main.java 	new file:   aula3/saque/Main.java

---
+++ aula3/filme/Main.java
@@ -0,0 +1,12 @@
+package aula3.filme;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        scan.close();
+    }
+
+}
