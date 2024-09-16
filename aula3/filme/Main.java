1dbe8c070f5c42026eb1c596983bbbe234abb920
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-15 21:01:40 -0300

    modified:   aula3/filme/Main.java

--- aula3/filme/Main.java
+++ aula3/filme/Main.java
@@ -18,7 +18,7 @@ class Filme {
         String nomeFilme = "";
 
         while (nomeFilme.isEmpty()) {
-            System.out.println("Digite uma string (não deixe em branco): ");
+            System.out.print("Qual o nome do filme?: ");
             nomeFilme = scan.nextLine();
         }
 
@@ -34,45 +34,64 @@ class Filme {
         Scanner scan = new Scanner(System.in);
         String genFilme = "";
 
-        while (true) {
-            System.out.println("Digite uma opção (Romance, Terror, Comédia): ");
-            genFilme = scan.nextLine().trim(); // Remove espaços em branco ao redor
+        while (genFilme.isEmpty()) {
 
-            if (genFilme.equalsIgnoreCase("Romance") ||
-                genFilme.equalsIgnoreCase("Terror") ||
-                genFilme.equalsIgnoreCase("Comédia")) {
+            if (genFilme == "1") {
 
-                    return genFilme;
+                genFilme = "Romance";
 
-            } else {
+            } else if (genFilme == "2") {
 
-                System.out.println("Entrada inválida. Tente novamente.");
+                genFilme = "Terror";
 
+            } else if (genFilme == "3") {
+                genFilme = "Comedia";
             }
-
-            scan.close();
-
         }
+
+        scan.close();
+        return genFilme;
+
     }
 
-    public String getGenero(){
+    public String getGenero() {
         return genero();
     }
 
+    private String duracao() {
+        int horas = duracao / 60;
+        int min = duracao % 60;
 
+        String horaFormat = String.format("%d:%02d", horas, min);
 
+        return horaFormat;
+    }
 
-
+    public String getDuracao() {
+        return duracao();
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String nome = "";
-        String genero = "";
-        int duracao = 0;
+        
+        System.out.println("Qual o nome do filme");
+        String nome = scan.nextLine();
+        
+        System.out.println("Digite uma opção:\n(1) Romance\n(2) Terror\n(3) Comedia): ");
+        String genero = scan.nextLine();
+        
+        System.out.println("qual a duração do filme?");
+        int duracao = scan.nextInt();
+        
         Filme filme = new Filme(nome, genero, duracao);
 
+        String nm = filme.getNome();
+        String gen = filme.getGenero();
+        String dur = filme.getDuracao();
+
+        System.out.println(nm + "\n" + gen + "\n" + dur);
 
         scan.close();
     }
