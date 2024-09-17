85ef4ca4c148cd418ef887732bda7338f489b4c4
Marcelo Lisboa <161672293+oLisboaaa@users.noreply.github.com>
2024-09-17 10:42:26 -0300

    Update Itau.java
    vesion 0.8
    
    made in OnlineGDB

--- aula3/saque/Itau.java
+++ aula3/saque/Itau.java
@@ -17,8 +17,20 @@ class Banco{
         return saque;
     }
     public void setSaque(double saque) {
+        Scanner scan = new Scanner(System.in);
         this.saque = saque;
+        double taxa = 0.005;
         
+        System.out.println("qual o valor a ser sacado?");
+        saque = scan.nextDouble();
+        
+        taxa = saque * taxa;
+        System.out.print("Sera cobrado uma taxa no valor de "+taxa);
+        
+        saque = this.saque + taxa;
+        
+        System.out.print("o valor total é de " + saque);
+        return saque;
     }
 
 
@@ -26,7 +38,21 @@ class Banco{
         return deposito;
     }
     public void setDeposito(double deposito) {
+        Scanner scan = new Scanner(System.in);
         this.deposito = deposito;
+        double taxa = 0.01;
+        
+        System.out.println("qual valor voce deseja depositar?");
+        this.deposito = scan.nextDouble();
+            
+        taxa = this.deposito * taxa;
+            
+        System.out.print("sera cobrado uma taxa de "+taxa);
+            deposito = deposito + taxa;
+            
+        System.out.print("O valor total é de "+deposito);
+        return deposito;
+        }
     }
 
 
@@ -35,6 +61,7 @@ class Banco{
     }
     public void setConsulta(double consulta) {
         this.consulta = consulta;
+        double taxa = 0.1;
     }
 
 
@@ -44,7 +71,6 @@ class Banco{
 public class Itau {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         scan.close();
     }
 
