76c6abfc6df586e3e6ded8d7415aee4ec75fd566
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-17 15:32:59 -0300

    modified:   aula3/saque/Itau.java

--- aula3/saque/Itau.java
+++ aula3/saque/Itau.java
@@ -18,7 +18,6 @@ class Banco{
     }
     public void setSaque() {
         Scanner scan = new Scanner(System.in);
-        this.saque = saque;
         double taxa = 0.005;
         
         System.out.println("qual o valor a ser sacado?");
@@ -29,8 +28,7 @@ class Banco{
         
         saque = this.saque + taxa;
         
-        System.out.print("o valor total é de " + saque);
-        return saque;
+        System.out.print("\n\no valor total é de " + saque);
     }
 
 
@@ -39,7 +37,6 @@ class Banco{
     }
     public void setDeposito() {
         Scanner scan = new Scanner(System.in);
-        this.deposito = deposito;
         double taxa = 0.01;
         
         System.out.println("qual valor voce deseja depositar?");
@@ -50,16 +47,16 @@ class Banco{
         System.out.print("sera cobrado uma taxa de "+taxa);
             deposito = deposito + taxa;
             
-        System.out.print("O valor total é de "+deposito);
-        return deposito;
+        System.out.print("\n\nO valor total é de "+deposito);
         }
     public double getConsulta() {
         return consulta;
     }
     public void setConsulta() {
-        this.consulta = consulta;
         double taxa = 0.1;
+
     }
+    
 }
 
 
@@ -71,6 +68,19 @@ class Banco{
 public class Itau {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Banco banco = new Banco(0, 0, 0);
+
+        System.out.println("qual operacao voce quer realiza:\n[1] saque\n[2] deposito\n[3] consulta de saldo");
+        int opcao = scan.nextInt();
+        if(opcao == 1){
+            banco.setSaque();
+        }else if(opcao == 2){
+            banco.setDeposito();
+        }else{
+            banco.setConsulta();
+        }
+
+        System.out.println("\n\n\nRESUMO DA SUA OPERACAO\n\nSaque: "+banco.getSaque()+"\nDeposito: "+banco.getDeposito()+"\nConsulta: "+banco.getConsulta());
         scan.close();
     }
 
