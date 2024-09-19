ff7caa32089362951f13bf01b89958df969ebab5
oLisboaaa <lisboaa.eu@gmail.com>
2024-09-18 23:51:56 -0300

    modified:   aula1/pessoa/Pessoa.java

--- aula1/pessoa/Pessoa.java
+++ aula1/pessoa/Pessoa.java
@@ -1,15 +1,17 @@
 package aula1.pessoa;
 
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 class gPessoa {
    String nome;
    String sobrenome;
-   String dataNasc;
-   int altura;
+   Date dataNasc;
+   double altura;
    String email;
 
-   public gPessoa(String nome, String sobrenome, String dataNasc, int altura, String email) {
+   public gPessoa(String nome, String sobrenome, Date dataNasc, double altura, String email) {
       this.nome = nome;
       this.sobrenome = sobrenome;
       this.dataNasc = dataNasc;
@@ -24,49 +26,65 @@ class gPessoa {
    public void setNomeCompleto() {
       Scanner scan = new Scanner(System.in);
 
-      System.out.println("qual o primeiro nome?");
+      System.out.println("Qual o primeiro nome?");
       nome = scan.nextLine();
 
-      System.out.println("\nqual o ultimo nome?");
+      System.out.println("\nQual o último nome?");
       sobrenome = scan.nextLine();
    }
 
-   public String getDataNasc() {
+   public Date getDataNasc() {
       return dataNasc;
    }
 
    public void setDataNasc() {
       Scanner scan = new Scanner(System.in);
       System.out.print("\n\nInsira a data de nascimento do usuário (ddMMyyyy / Somente números): ");
-      dataNasc = scan.nextLine();
+      String dataStr = scan.nextLine();
 
+      try {
+         SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+         this.dataNasc = sdf.parse(dataStr);
+
+         // Verifica se a data de nascimento é anterior à data atual
+         if (dataNasc.after(new Date())) {
+            System.out.println("Data inválida. A data de nascimento não pode ser posterior à data atual.");
+            setDataNasc(); // Chama novamente se a data for inválida
+         }
+      } catch (Exception e) {
+         System.out.println("Data inválida. Tente novamente.");
+         setDataNasc(); // Chama novamente se a data for inválida
+      }
    }
 
-   public int getAltura() {
+   public double getAltura() {
       return altura;
    }
 
    public void setAltura() {
       Scanner scan = new Scanner(System.in);
-      System.out.println("\n\nQual a altura?");
+      System.out.println("\n\nQual a altura? (EM CENTIMETROS)");
       altura = scan.nextInt();
-
+      altura /= 100; // Converte para metros
    }
 
    public String getEmail() {
-      return email;
+      return email.toLowerCase();
    }
 
    public void setEmail() {
       email = nome + "." + sobrenome + "@facens.br";
    }
 
+   public String formatarDataNasc() {
+      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+      return sdf.format(dataNasc);
+   }
 }
 
 public class Pessoa {
    public static void main(String[] args) {
-
-      gPessoa gpessoa = new gPessoa("", "", "", 0, "");
+      gPessoa gpessoa = new gPessoa("", "", null, 0, "");
 
       gpessoa.setNomeCompleto();
       gpessoa.setDataNasc();
@@ -75,9 +93,8 @@ public class Pessoa {
 
       System.out.println(
             "\n\nNome: " + gpessoa.getNomeCompleto() +
-            "\nData de nascimento: " + gpessoa.getDataNasc() +
-            "\nAltura: " + gpessoa.getAltura() +
-            "\nEmail: " + gpessoa.getEmail());
-
+                  "\nData de nascimento: " + gpessoa.formatarDataNasc() +
+                  "\nAltura: " + gpessoa.getAltura() + " M" +
+                  "\nEmail: " + gpessoa.getEmail());
    }
 }
